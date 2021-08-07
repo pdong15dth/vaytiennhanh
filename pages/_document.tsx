dfa6d3846367b8e6fee79bbc36c788314b7527cf
pdong15dth <pdong15dth06@gmail.com>
2021-08-07 22:49:00 +0700

    fix https

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -17,7 +17,7 @@ export default class MyDocument extends Document {
           <Main />
           <NextScript />
             <Script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></Script>
-            <Script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.11.1/jquery.validate.min.js"></Script>
+            <Script src="https://ajax.aspnetcdn.com/ajax/jquery.validate/1.11.1/jquery.validate.min.js"></Script>
         </body>
       </Html>
     );
